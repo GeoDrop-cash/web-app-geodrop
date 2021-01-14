c1229b4dd43e6e636ee81580e01a5d3874ac8521
Daniel Gonzalez <danielhumgon@gmail.com>
2021-01-14 15:39:40 -0400

    Rearranged menu and changed footer

---
+++ src/gatsby-ipfs-web-wallet/components/footer/index.js
@@ -0,0 +1,41 @@
+/*
+  This is an example of using Gatsby 'Component Shadowing' to overwrite the
+  default Wallet View. It injects the same TX History component at the bottom
+  of the Wallet View.
+*/
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Row, Col, Content, Box, Button, Inputs } from 'adminlte-2-react'
+import 'gatsby-ipfs-web-wallet/src/components/footer/footer.css'
+import './footer.css'
+// import Footer from 'gatsby-ipfs-web-wallet/src/components/footer'
+class Footer2 extends React.Component {
+  // class Wallet2 extends React.Component {
+  constructor (props) {
+    super(props)
+    console.log('Loading new example view.')
+
+    // console.log('Wallet info: ', props.walletInfo)
+  }
+
+  render () {
+    return (
+      <>
+        <section id='footer2'>
+          <Row className='footer-container'>
+            <Col md={12} className='footer-section'>
+              <small><strong>Sponsored By:</strong> FullStack.cash</small>
+            </Col>
+          </Row>
+        </section>
+      </>
+    )
+  }
+
+  async componentDidMount () {
+
+  }
+}
+
+export default Footer2
--- src/gatsby-ipfs-web-wallet/components/menu-components.js
+++ src/gatsby-ipfs-web-wallet/components/menu-components.js
@@ -37,46 +37,6 @@ const { Item } = Sidebar
 const MenuComponents = props => {
   return [
     {
-      key: 'Tokens',
-      component: <Tokens key='Tokens' {...props} />,
-      menuItem: <Item icon='fas-coins' key='Tokens' text='Tokens' />
-    },
-    {
-      key: 'Send/Receive BCH',
-      component: <SendReceive key='Send/Receive BCH' {...props} />,
-      menuItem: (
-        <Item
-          icon='fa-exchange-alt'
-          key='Send/Receive BCH'
-          text='Send/Receive BCH'
-        />
-      )
-    },
-    {
-      key: 'Wallet',
-      component: <Wallet key='Wallet' {...props} />,
-      menuItem: <Item icon='fa-wallet' key='Wallet' text='Wallet' />
-    },
-    {
-      key: 'Configure',
-      component: <Configure key='Configure' {...props} />,
-      menuItem: <Item icon='fas-cog' key='Configure' text='Configure' />
-    },
-    {
-      key: 'TX History',
-      component: <TXHistory key='TX History' {...props} />,
-      menuItem: (
-        <Item icon='fas-cog' key='TX History' text='TX History' />
-      )
-    },
-    {
-      key: 'Demo Component',
-      component: <DemoComponent key='Demo Component' {...props} />,
-      menuItem: (
-        <Item icon='fas-cog' key='Demo Component' text='Demo Component' />
-      )
-    },
-    {
       key: 'CashDrop',
       component: <CashDrop key='CashDrop' {...props} />,
       menuItem: (
@@ -96,6 +56,47 @@ const MenuComponents = props => {
       component: <Play key='Play' {...props} />,
       menuItem: (
         <Item icon='fas-cog' key='Play' text='Play' />
+      )
+    },
+    {
+      key: 'Tokens',
+      component: <Tokens key='Tokens' {...props} />,
+      menuItem: <Item icon='fas-coins' key='Tokens' text='Tokens' />
+    },
+
+    {
+      key: 'Wallet',
+      component: <Wallet key='Wallet' {...props} />,
+      menuItem: <Item icon='fa-wallet' key='Wallet' text='Wallet' />
+    },
+    {
+      key: 'Configure',
+      component: <Configure key='Configure' {...props} />,
+      menuItem: <Item icon='fas-cog' key='Configure' text='Configure' />
+    },
+    {
+      key: 'Send/Receive BCH',
+      component: <SendReceive key='Send/Receive BCH' {...props} />,
+      menuItem: (
+        <Item
+          icon='fa-exchange-alt'
+          key='Send/Receive BCH'
+          text='Send/Receive BCH'
+        />
+      )
+    },
+    {
+      key: 'TX History',
+      component: <TXHistory key='TX History' {...props} />,
+      menuItem: (
+        <Item icon='fas-cog' key='TX History' text='TX History' />
+      )
+    },
+    {
+      key: 'Demo Component',
+      component: <DemoComponent key='Demo Component' {...props} />,
+      menuItem: (
+        <Item icon='fas-cog' key='Demo Component' text='Demo Component' />
       )
     },
     {
