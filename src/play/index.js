ac44c9353aeffecc92be41d56ba484c6de5c8d5e
Daniel Gonzalez <danielhumgon@gmail.com>
2021-01-16 16:42:22 -0400

    Play View: Create text box

--- src/play/index.js
+++ src/play/index.js
@@ -7,28 +7,95 @@
 */
 
 import React from 'react'
-import { Content } from 'adminlte-2-react'
+import { Content, Row, Col, Box, Button } from 'adminlte-2-react'
+import './play.css'
 
+let _this
 class Play extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-
+      output: '',
+      showTerminal: true,
+      foundDrop: false
     }
+    _this = this
   }
 
   render () {
+    const { showTerminal, output, dropFound } = _this.state
     return (
       <Content
         title='Play'
         subTitle='Play'
         browserTitle='Play'
-      />
+      >
+        <Row>
+          <Col xs={12}>
+            <Box className="border-none">
+              <span >
+                {showTerminal && (
+                  <textarea
+                    id="playTerminal"
+                    className='playTerminal'
+                    name="ipfsTerminal"
+                    rows="15"
+                    cols="50"
+                    readOnly
+                    value={`${output ? `${output}>` : '>'}`}
+                  />
+                )}
+              </span>
+            </Box>
+          </Col>
+          <Col xs={12} className="text-center">
+            <Button
+              text='Collect'
+              type='primary'
+              className='btn-lg'
+              disabled={!dropFound}
+            />
+          </Col>
+        </Row>
+
+      </Content>
     )
   }
 
   componentDidMount () {
+    _this.handleDrop()
+  }
+
+  // Adds a line to the terminal
+  handleLog (str) {
+    try {
+      _this.setState({
+        output: _this.state.output + '   ' + str + '\n'
+      })
+      _this.keepScrolled()
+    } catch (error) {
+      console.warn(error)
+    }
+  }
+
+  // Keeps the terminal scrolled to the last line
+  keepScrolled () {
+    try {
+    // Keeps scrolled to the bottom
+      var textarea = document.getElementById('playTerminal')
+      if (textarea) {
+        textarea.scrollTop = textarea.scrollHeight
+      }
+    } catch (error) {
+      console.warn(error)
+    }
+  }
+
+  handleDrop () {
+    setInterval(() => {
+      _this.handleLog('No pins close by')
+    }, 1000)
   }
 }
 
